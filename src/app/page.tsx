8095c6d440ceca7867c66c3e7800403a1b139725
azibabc121 <azibabc121@gmail.com>
2024-07-31 02:27:40 +0500

    Featured courses section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-while/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
